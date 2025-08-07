76fc31c69cda29cc94edd4432af967ab4c559e4c
Marcos Vogado <marcoshcvogado@gmail.com>
2025-08-07 14:30:59 -0300

    Replace default Angular template with custom content
    Removed the default Angular placeholder template from app.component.html and replaced it with a simple header and router outlet. Updated the title property in app.component.ts to 'Meu Primeiro Projeto Angular' for a more personalized app name.

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'meu-primeiro-projeto';
+  
+  public title = 'Meu Primeiro Projeto Angular';
+
 }
