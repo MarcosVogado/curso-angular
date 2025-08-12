d65d16dbfe5150e18c6bd692474225d2d6d39aa4
Marcos Vogado <marcoshcvogado@gmail.com>
2025-08-12 13:45:45 -0300

    Add DataBindingComponent and update AppComponent
    Introduced DataBindingComponent with its template, styles, and test file. Updated AppComponent to use DataBindingComponent and removed unused lifecycle hooks and methods. Registered DataBindingComponent in AppModule.

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -1,55 +1,18 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template:`
-  <button (click)="destruirComponent()">Destruir componente</button>
-  <br>
-  {{valor}}
-  <button (click)="adicionar()">Adicionar</button>
-  <app-title *ngIf="destruir" ></app-title>
-  1<router-outlet></router-outlet>
+  <app-data-binding></app-data-binding>
+  <router-outlet></router-outlet>
   `,
 })
-export class AppComponent implements OnInit, DoCheck, AfterContentChecked, AfterContentInit, AfterViewInit, AfterViewChecked {
-
-  public destruir:boolean = true;
-
-  public valor: number = 1;
-
+export class AppComponent implements OnInit {
+  
   constructor() { }
-
-  public destruirComponent(): void {
-    this.destruir = !this.destruir;
-  }
-
-  public adicionar(): number{
-    return this.valor++;
-  }
 
   ngOnInit(): void {
     
   }
-
-  ngDoCheck(): void {
-    console.log("Do Check");
-  }
-
-  ngAfterContentInit(): void {
-    console.log("After Content Init");
-  }
-
-  ngAfterContentChecked(): void {
-    console.log("After Content Checked");
-  }
-
-  ngAfterViewInit(): void {
-    console.log("After View Init");
-  }
-
-  ngAfterViewChecked(): void {
-    console.log("After View Checked");
-  }
-
 
 }
