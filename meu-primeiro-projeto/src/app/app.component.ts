df4c91e257a6757915c8d246300248c3527a6136
Marcos Vogado <marcoshcvogado@gmail.com>
2025-08-14 17:48:46 -0300

    estudando mais

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -11,13 +11,18 @@ import { Component, OnInit } from '@angular/core';
   <app-diretivas-estruturais></app-diretivas-estruturais>
   <app-data-binding></app-data-binding>
   <app-diretivas-atributos></app-diretivas-atributos>
+  <app-new-component></app-new-component>
   -->
 
-  <app-new-component></app-new-component>
+  <app-input [contador]="addValue"></app-input>
+  <br>
+  <button (click)="add()">Add</button>
 
   <router-outlet></router-outlet>`,
 })
 export class AppComponent implements OnInit {
+
+  public addValue: number = 0;
 
   constructor() { }
 
@@ -25,4 +30,7 @@ export class AppComponent implements OnInit {
   
   }
 
+  add() {
+    this.addValue++;
+  }
 }
